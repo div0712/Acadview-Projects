afff87b680db4dbb18b5656cc0ce6a4e15417099
divyesh <divyeshsharma07@gmail.com>
2017-07-09 16:02:17 +0530

    final

--- pro.py
+++ pro.py
@@ -82,7 +82,6 @@ def addstatus(spy_name):
                 return ("null")
             else:
                 return (position)
-
             def send_a_message(spy_name):
                 global count
                 position = select_a_friend(
@@ -103,8 +102,7 @@ def addstatus(spy_name):
                     from datetime import datetime
                     date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
                     count = count + 1
-                    spy_detail[spy_name]["friends"][f_name]["chat"].update({count: {"text": text, "time": date,
-                                                                                    "boolean": True}})
+                    spy_detail[spy_name]["friends"][f_name]["chat"].update({count: {"text": text, "time": date,   "boolean": True}})
                     print "please wait..................................."
                     s = text.strip().split(" ")
                     Steganography.encode(path, output_path, text)
@@ -121,3 +119,114 @@ def addstatus(spy_name):
                         return (1)
                     else:
                         return (0)
+
+                    def read_a_message(spy_name):
+                        global count
+                        position = select_a_friend(
+                            spy_name)
+                        if position == "null":
+                            return ()
+                        else:
+                            from steganography.steganography import Steganography
+                            f_name = spy_detail[spy_name]["friends"].keys()[position]
+                            print "Friend %s selected\n" % (f_name)
+                            o_img_name = raw_input(
+                                "What is the name of the output file you want to decode?(Type along with extension): ")
+                            o_path = "C:\Users\Divyesh\PycharmProjects\ACADVIEW\ACADVIEW PROJECTS\\"
+                            output_path = o_path + o_img_name
+                            from datetime import datetime
+                            date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                            print "please wait...................................."
+                            secret_text = Steganography.decode(output_path)
+                            if len(secret_text) == 0:
+                                print "Image has no secret message"
+                            else:
+                                print "\nYour secret text is: " + secret_text
+                                count = count + 1
+                                spy_detail[spy_name]["friends"][f_name]["chat"].update({count: {"text": secret_text, "time": date, "boolean": False}})
+                    def chat_read(spy_name):
+                        position = select_a_friend(
+                            spy_name)
+                        if position == "null":
+                            return ()
+                        f_name = spy_detail[spy_name]["friends"].keys()[
+                            position]
+                        print "Chat history is as follows:\n"
+                        print spy_detail[spy_name]["friends"][f_name]["chat"]
+                    while True:
+                        while True:
+                            user_choice = int(raw_input(
+                                "Do you want to continue using a:\n1. Default spy.\n2. Create new spy.\nEnter 1 or 2: "))
+                            if user_choice == 1:
+                                import spy_details
+                                spy_name = spy_details.default_spy_detail.keys()[0]
+                                if spy_name in spy_detail.keys():
+                                    print "Default user name already exists in the spy dictionary"
+                                else:
+                                    spy_detail.update(
+                                        spy_details.default_spy_detail)
+                                    spy_status_history.update({spy_name: []})
+                            elif user_choice == 2:
+                                spy_name = raw_input("Login:\nEnter your spy name: ")
+                                if len(spy_name) < 1:
+                                    print "\nYour name is invalid. Try again.\n"
+                                    continue
+                                else:
+                                    sal = int(raw_input("What is your salutaion?\n1. Mr\n2. Ms\nEnter 1 or 2: "))
+                                    if sal == 1:
+                                        salutation = "Mr"
+                                    elif sal == 2:
+                                        salutation = "Ms"
+                                    else:
+                                        print "Wrong Input.\n"
+                                        continue
+                                    age = int(raw_input("What is your age %s %s?\n" % (salutation, spy_name)))
+                                    if age >= 12 and age <= 50:  # checks for valid age
+                                        rating = float(raw_input("What is your rating?\n"))
+                                        if rating < 0:
+                                            print "Rating cannot be negative."
+                                            break
+                                        elif rating > 10:
+                                            print "Rating cannot be grater than 10."
+                                            break
+                                        else:
+                                            print "Rating has been accepted.\n"
+                                        spy_detail.update({spy_name: {"salutation": salutation, "age": age,
+                                                                      "rating": rating, "friends": {},
+                                                                      "status": ""}})
+                                        spy_status_history.update(
+                                            {spy_name: []})
+                                    else:
+                                        print "Sorry, we can't authenticate your age.....try again"
+                                        continue
+                            else:
+                                print "Wrong input. Try again\n"
+                                continue
+                            if spy_name in spy_detail.keys():
+                                print "\nWelcome %s %s!" % (
+                                spy_detail[spy_name]["salutation"], spy_name)
+                                print "Your age: %i" % (spy_detail[spy_name]["age"]) + "\nYour Rating is: %.2f\n" % (
+                                spy_detail[spy_name]["rating"])
+                                while True:
+                                    menu = int(raw_input(
+                                        "Menu:\n1. Add a status update\n2. Add a friend\n3. Send a secret message\n4. Read a secret message\n5. Read chats from a user\n6. Exit\nEnter from 1,2,3,4,5 or 6: "))  # menu options
+                                    if menu == 1:
+                                        add_a_status(spy_name)
+                                    elif menu == 2:
+                                        friends_count = add_friend(spy_name)
+                                        print "You have %i friends as of now.\n" % (friends_count)
+                                    elif menu == 3:
+                                        spy_del = send_a_message(spy_name)
+                                        if spy_del == 1:
+                                            break
+                                    elif menu == 4:
+                                        read_a_message(spy_name)
+                                    elif menu == 5:
+                                        chat_read(spy_name)
+                                    elif menu == 6:
+                                        print "The program will now Exit."
+                                        exit()  # exits the program
+                                    else:
+                                        print "Wrong input. Try again"
+                                    continue
+                    exit()
